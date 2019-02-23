de81dfabde907d9878f51b4406d2470f3f37ccb3
jose <jecm3099@gmail.com>
2019-02-23 13:37:10 -0400

    se agrego la funcion suma

---
+++ WSaritmetica/WCFaritmetica/srvSuma.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFaritmetica
+{
+
+    public class srvSuma : IsrvSuma
+    {
+        public clsSuma suma(int numero1, int numero2)
+        {
+            clsSuma obj = new clsSuma();
+            obj.numeroA = numero1;
+            obj.numerob = numero2;
+            obj.resultado = obj.numeroA + obj.numerob;
+
+
+            return obj;
+
+
+        }
+    }
+}
